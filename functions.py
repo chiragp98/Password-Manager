2afacfd8c4425882d49a3e29af52b5230b1c8fbd
Chirag <chirag@Tests-MacBook-Pro.local>
2022-10-17 20:02:31 +0100

    Encrpytion + Password Functionality

--- functions.py
+++ functions.py
@@ -1,15 +1,35 @@
-def display():
+from cryptography.fernet import Fernet
+
+
+def display(fer):
     with open ('passwords.txt', 'r') as f:
         for line in f.readlines():
             data = line.rstrip()
             sitename, user, pwd = data.split('|')
-            print('Site: ' + sitename,' User: '  + user, ' Password: ' + pwd,'\n')
+            print('Site: ' + sitename,' User: '  + user, ' Password: ' + str(fer.decrypt(pwd[1:])),'\n')
 
 
-def pw_entry():
+def pw_entry(fer):
     Site = input('Site Name: ')
     Username = input('Username: ')
     Password = input('Password: ')
 
     with open ('passwords.txt', 'a') as f:
-        f.write(Site + '|' + Username + '|' + Password + '\n')
+        f.write(Site + '|' + Username + '|' + str(fer.encrypt(Password.encode())) + '\n')
+
+### Function to create key 
+'''
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+
+write_key()
+'''
+## load key for master pw
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
--- password_manager.py
+++ password_manager.py
@@ -1,23 +1,24 @@
 ### Password Manager
-from functions import pw_entry, display
 
-MasterPWD = input("Master Password: ")
+from functions import pw_entry, display, load_key
+from cryptography.fernet import Fernet
 
-
-while True:
-    mode = input("Please select the mode you would like to use:   add    view    exit" '\n' " :").lower()
-    if mode == 'exit':
-        break
-    elif mode == 'add':
-        pw_entry()
-    elif mode == 'view':
-        display()
-    else:
-        print("Invalid Input")
-        continue
+UserPWD = input('Enter Password to Access: ')
+MasterPWD = "Hello"
+key = load_key() + MasterPWD.encode()
+user_key = load_key() + UserPWD.encode()
+fer = Fernet(key)
 
 
-
-
-
-
+if user_key == key :
+    while True:
+        mode = input("Please select the mode you would like to use:   add    view    exit" '\n' " :").lower()
+        if mode == 'exit':
+            break
+        elif mode == 'add':
+            pw_entry(fer)
+        elif mode == 'view':
+            display(fer)
+        else:
+            print("Invalid Input")
+            continue
